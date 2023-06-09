461b4cdc70af6886d2d3d520343afc0dc924821f
matheus55391 <matheus.manulo55@gmail.com>
2023-06-09 11:42:52 -0300

    criando layer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,37 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white lg:px-6">
       <div className="lg:p-2">
-        <p className="pl-1 lg:pl-2 text-sm md:text-2xl lg:text-3xl font-bold mb-4 text-gray-300 uppercase text-left">{"<"}Olá, eu sou Matheus{"/>"}</p>
+        <p className="pl-1 lg:pl-2 text-sm md:text-2xl lg:text-3xl mb-4 .text-gray-bold-uppercase text-left">{"<"}Olá, eu sou Matheus{"/>"}</p>
         <div className="flex flex-row lg:items-center pb-1">
-          <p className="pl-[1px] text-5xl md:text-7xl lg:text-9xl font-bold text-gray-300 uppercase lg:text-center" style={{ lineHeight: "0.8" }}>
+          <p className="pl-[1px] text-5xl md:text-7xl lg:text-9xl .text-gray-bold-uppercase lg:text-center" style={{ lineHeight: "0.8" }}>
             FULLSTACK
           </p>
           <div className="hidden lg:visible lg:flex space-x-2 lg:space-x-4 mt-4 md:mt-0">
-            <a href="https://github.com/matheus55391" target="_blank" rel="noopener noreferrer" className="text-[16px] lg:text-[32px]">
+            <a href="https://github.com/matheus55391" target="_blank" rel="noopener noreferrer" className="text-[16px] lg:text-[32px] icon-blue-pulse">
               <FaGithub />
             </a>
-            <a href="https://www.linkedin.com/in/matheus-felipe-vieira-santiago-5a321a208/" target="_blank" rel="noopener noreferrer" className="text-[16px] lg:text-[32px]">
+            <a href="https://www.linkedin.com/in/matheus-felipe-vieira-santiago-5a321a208/" target="_blank" rel="noopener noreferrer" className="text-[16px] lg:text-[32px] icon-blue-pulse">
               <FaLinkedin />
             </a>
-            <a href="mailto:matheus.felipe55391@gmail.com" className="text-[16px] lg:text-[32px]">
+            <a href="mailto:matheus.felipe55391@gmail.com" className="text-[16px] lg:text-[32px] icon-blue-pulse">
               <FaEnvelope />
             </a>
           </div>
         </div>
-        <p className="text-6xl md:text-10xl lg:text-11xl font-bold text-gray-300 uppercase text-left mb-2" style={{ lineHeight: "0.8" }}>
+        <p className="text-6xl md:text-10xl lg:text-11xl .text-gray-bold-uppercase text-left mb-2" style={{ lineHeight: "0.8" }}>
           DEVELOPER
         </p>
         <p className="pl-1 uppercase lg:text-right text-[10px] lg:text-[16px]">3 anos de experiência como desenvolvedor full stack.</p>
 
       </div>
       <div className="flex lg:hidden space-x-2 lg:space-x-4 pt-12 ">
-            <a href="https://github.com/matheus55391" target="_blank" rel="noopener noreferrer" className="text-[26px] ">
+            <a href="https://github.com/matheus55391" target="_blank" rel="noopener noreferrer" className="text-[26px] icon-blue-pulse">
               <FaGithub />
             </a>
-            <a href="https://www.linkedin.com/in/matheus-felipe-vieira-santiago-5a321a208/" target="_blank" rel="noopener noreferrer" className="text-[26px] ">
+            <a href="https://www.linkedin.com/in/matheus-felipe-vieira-santiago-5a321a208/" target="_blank" rel="noopener noreferrer" className="text-[26px] icon-blue-pulse">
               <FaLinkedin />
             </a>
-            <a href="mailto:matheus.felipe55391@gmail.com" className="text-[26px] ">
+            <a href="mailto:matheus.felipe55391@gmail.com" className="text-[26px] icon-blue-pulse">
               <FaEnvelope />
             </a>
         </div>
