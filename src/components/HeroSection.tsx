0d5c5d2b8ef1cc8b34019c51479fd521cee4439c
matheus55391 <matheus.felipe55391@gmail.com>
2024-06-18 23:10:55 -0300

    melhoria heroSection tamanho do icone

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section className="flex flex-col md:flex-row justify-between items-center container mx-auto 2xl:px-48 xl:px-24  my-16 md:my-56 ">
-      <div className="rounded-full bg-purple-500 w-60 h-60  md:hidden flex items-center justify-center overflow-hidden mt-8 md:mt-0 ">
+      <div className="rounded-full bg-purple-500 min-w-60 h-60  md:hidden flex items-center justify-center overflow-hidden mt-8 md:mt-0 ">
         <Image
           src="/profile.jpg"
           alt="Matheus Felipe"
@@ -20,12 +20,12 @@ const HeroSection = () => {
           I build things for web
         </h1>
       </div>
-      <div className="rounded-full bg-purple-500 min-w-60 h-60 hidden md:flex items-center justify-center overflow-hidden mt-8 md:mt-0 ">
+      <div className="rounded-full bg-purple-500 min-w-80 max-w-80 h-80 hidden md:flex items-center justify-center overflow-hidden mt-8 md:mt-0 ">
         <Image
           src="/profile.jpg"
           alt="Matheus Felipe"
-          width={228}
-          height={228}
+          width={308}
+          height={308}
           className="rounded-full"
         />
       </div>
